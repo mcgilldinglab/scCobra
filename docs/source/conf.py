d8ff469c270d9133edb336b7f43399502ac107cd
GlancerZ <93135125+GlancerZ@users.noreply.github.com>
2025-01-13 16:35:33 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'scCobra'
+copyright = '2024, Mcgilldinglab'
+author = 'Bowen Zhao'
 
 release = '0.1'
 version = '0.1.0'
